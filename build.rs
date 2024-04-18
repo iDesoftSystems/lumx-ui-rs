66a1b45ea0b5b8ec0a3a7298352246e572ab33c1
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2024-04-18 12:58:21 +0200

    chore: lumx ui inside style

--- build.rs
+++ build.rs
@@ -1,34 +1,32 @@
 use std::{
-    env::{self, VarError},
-    fs::{self, File},
-    io::Write,
+    env::{self},
+    fs::{self},
     path::{Path, PathBuf},
-    process::Command,
 };
 
 use anyhow::Context;
 
 pub fn main() {
-    let target_dir = env::var("OUT_DIR").expect("could not find 'out_dir'");
-    let cargo_target_dir = get_cargo_target_dir(Path::new(&target_dir)).unwrap();
+    let out_dir = env::var("OUT_DIR").expect("could not find 'out_dir'");
 
-    // let root_dir = Path::new(&target_dir).parent().unwrap();
-    let root_dir = cargo_target_dir.parent().unwrap();
+    let cargo_target_dir =
+        get_cargo_target_dir(Path::new(&out_dir)).expect("could not find 'target_dir'");
 
-    let style_dest_path = Path::new(&target_dir).join("lumx.css");
-    let mut style_f = File::create(&style_dest_path).unwrap();
-    style_f
-        .write_all(b".lumx-card { color: #000000; } ")
-        .unwrap();
+    let root_dir = cargo_target_dir
+        .parent()
+        .expect("could not find 'root_dir'");
 
-    let root_ui_path = root_dir.join("lumx-ui.css");
-    // let mut ui_f = File::create(&root_ui_path).unwrap();
-    fs::copy("./assets/lumx-ui.css", root_ui_path).unwrap();
+    let root_style_path = root_dir.join("style").join("lumx");
+    let root_style_ui_path = root_style_path.join("lumx-ui.scss");
+
+    fs::create_dir_all(&root_style_path).unwrap();
+    fs::copy("./assets/lumx-ui.css", root_style_ui_path).unwrap();
 }
 
 fn get_cargo_target_dir(out_dir: impl AsRef<Path>) -> anyhow::Result<PathBuf> {
     let mut target_dir = None;
     let mut sub_path = out_dir.as_ref();
+
     while let Some(parent) = sub_path.parent() {
         if parent.ends_with("target") {
             target_dir = Some(parent);
@@ -42,5 +40,6 @@ fn get_cargo_target_dir(out_dir: impl AsRef<Path>) -> anyhow::Result<PathBuf> {
             out_dir.as_ref().display()
         )
     })?;
+
     Ok(target_dir.to_path_buf())
 }
