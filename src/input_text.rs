0babf6d59f218003a2ed592c5780969f54b1faaf
Edinson Jim <4842086+edinsonjim@users.noreply.github.com>
2024-04-23 10:49:02 +0200

    feat: input text value

--- src/input_text.rs
+++ src/input_text.rs
@@ -1,17 +1,29 @@
-use leptos::{component, view, IntoView};
+use leptos::{component, event_target_value, view, IntoView, RwSignal, SignalSet};
 use uuid::Uuid;
 
 #[component]
 pub fn InputText(
     #[prop(default = "label")] label: &'static str,
     #[prop(default = "")] placeholder: &'static str,
+    value: RwSignal<String>,
 ) -> impl IntoView {
     let form_id = Uuid::new_v4().to_string();
+
+    // notify the new value when the element loses focus
+    let on_blur = move |ev| {
+        value.set(event_target_value(&ev));
+    };
 
     view! {
         <div class="lumx-input-text flex flex-col rounded-md border box-border border-slate-200 p-2.5 bg-white mb-2.5 focus-within:border focus-within:border-blue-600">
             <label for=form_id.clone() class="text-xs text-slate-900">{label}</label>
-            <input class="outline-none text-sm bg-white text-slate-900" placeholder=placeholder id=form_id type="text" value=""/>
+            <input
+                class="outline-none text-sm bg-white text-slate-900 placeholder:text-sm placeholder:text-slate-400"
+                type="text"
+                placeholder=placeholder
+                id=form_id
+                on:blur=on_blur
+                prop:value=value/>
         </div>
     }
 }
